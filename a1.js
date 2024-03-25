cd10f74c285c6c3fa6cfbb7df978cf58c110d853
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-03-25 21:29:06 +0500

    push on github

---
+++ a1.js
@@ -0,0 +1,25 @@
+var studentList = [
+    "aliza: gr.2066",
+    "saima: gr.6088",
+    "arisha: rr.4099",
+    "ali: gr.4444",
+    "ahmed: gr.3098",
+];
+console.log(studentList[3]);
+//my information
+var MyInformation = {
+    name: "saima",
+    fatherName: "abid",
+    age: 23,
+    city: "karachi",
+    education: "M.A",
+};
+console.log(MyInformation);
+console.log(MyInformation.city);
+//laptop
+var laptopRupees = [
+    ["dell, 22000"],
+    ["lenovo, 35000"],
+    ["hp, 400000"],
+];
+console.log(laptopRupees[0]);
---
+++ a1.ts
@@ -0,0 +1,27 @@
+let studentList:string[] = [
+    "aliza: gr.2066" ,
+     "saima: gr.6088",
+     "arisha: rr.4099",
+   "ali: gr.4444" ,
+     "ahmed: gr.3098",
+  ];
+  console.log(studentList[3]);
+  
+  //my information
+  
+  let MyInformation= {
+    name: "saima",
+    fatherName: "abid",
+    age: 23,
+    city: "karachi",
+    education: "M.A",
+  };
+  console.log(MyInformation);
+  console.log(MyInformation.city);
+  //laptop
+  let laptopRupees = [
+    ["dell, 22000"],
+    ["lenovo, 35000"],
+    ["hp, 400000"],
+  ];
+  console.log(laptopRupees[0]);
