7aecf22dae62a03fcb1eb8fd89d7c060429af3c0
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-04-20 01:13:31 +0500

    push on git

--- a4.js
+++ a4.js
@@ -1,8 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function myHoliday(name) {
+//example1
+let dayOff = "sunday";
+function myHoliday() { console.log(`today is ${dayOff} so i go for hiking`); }
+myHoliday();
+//example2
+function myName(name) {
     console.log(name.toUpperCase()),
         console.log(name.toLowerCase());
 }
-myHoliday("aliza");
-myHoliday("abid");
+myName("aliza abid");
+//example3
+function myInfo(name, qualification, age, city, now) {
+    console.log(` My name is ${name} . I completed a ${qualification}. My age is 
+    ${age}. I live in ${city}. Today ${now}`);
+}
+myInfo("Aliza Abid", "Masters in psychology", 22, "karachi", " I started a coding and and also practice it");
+function myInformation(fullName, age, isMarried, cnic, phoneNumber) {
+    let information = { fullName, age, isMarried, cnic, phoneNumber };
+    return information;
+}
+let user1 = myInformation("Aliza Abid", 22, false, 78878, 967532356);
+console.log(user1);
--- a4.ts
+++ a4.ts
@@ -1,5 +1,42 @@
-function myHoliday(name:string){console.log(name.toUpperCase()),
+//example1
+let dayOff:string = "sunday";
+function  myHoliday(){console.log(`today is ${dayOff} so i go for hiking`)}
+ myHoliday()
+
+
+ //example2
+ function myName(name:string){console.log(name.toUpperCase()),
     console.log(name.toLowerCase())}
 
- myHoliday("aliza")
- myHoliday("abid")
+ myName("aliza abid")
+
+
+ //example3
+ function myInfo(name:string,
+    qualification:string,age:number,city:string,now?:string
+
+ ){console.log(` My name is ${name} . I completed a ${qualification}. My age is 
+    ${age}. I live in ${city}. Today ${now}`)}
+    myInfo("Aliza Abid", "Masters in psychology", 22,"karachi", " I started a coding and and also practice it")
+
+
+ //example 4
+ interface informationtype {fullName:string,
+    age:number,
+isMarried:boolean,
+cnic?:number,
+phoneNumber: number
+}
+
+function myInformation(fullName:string,
+    age:number,
+isMarried:boolean,
+cnic:number,
+phoneNumber: number):informationtype{
+   let information:informationtype = {fullName, age,isMarried,cnic, phoneNumber}
+   return information
+}
+
+let user1 = myInformation ("Aliza Abid", 22, false, 78878 , 967532356)
+console.log(user1)
+
